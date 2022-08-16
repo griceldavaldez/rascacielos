1b51b100efd37a67787dcaa0b1ab529ac6bf1b17
griceldavaldez <gricel.va99@gmail.com>
2022-08-16 11:01:25 -0400

    Agrega pistas del juego

---
+++ pistas.pl
@@ -0,0 +1,41 @@
+%   F  I   L   A   S
+
+%Horizontal izquierda
+pista(horizontal,1,izquierda,3).
+pista(horizontal,2,izquierda,2).
+pista(horizontal,3,izquierda,3).
+pista(horizontal,4,izquierda,2).
+pista(horizontal,5,izquierda,1).
+
+%Horizontal derecha
+pista(horizontal,1,derecha,3).
+pista(horizontal,2,derecha,4).
+pista(horizontal,3,derecha,1).
+pista(horizontal,4,derecha,2).
+pista(horizontal,5,derecha,2).
+
+
+
+%   C   O   L   U   M   N   A   S
+
+%Vertical arriba
+pista(vertical,1,arriba,4).
+pista(vertical,2,arriba,2).
+pista(vertical,3,arriba,1).
+pista(vertical,4,arriba,2).
+pista(vertical,5,arriba,3).
+
+%Vertical abajo
+pista(vertical,1,abajo,1).
+pista(vertical,2,abajo,4).
+pista(vertical,3,abajo,3).
+pista(vertical,4,abajo,2).
+pista(vertical,5,abajo,2).
+
+
+
+
+
+
+
+   
